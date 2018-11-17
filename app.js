bc4a88d96f6efe4bd1be060862ac7b8050aace62
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2018-11-17 15:19:19 -0500

    app.js config

--- app.js
+++ app.js
@@ -1,12 +1,60 @@
-const express = require('express');
+const express               = require('express')
+      // Models:
+      Post                  = require('./models/Post'),
+      User                  = require('./models/User'),
+      passport              = require('passport'),
+      LocalStrategy         = require('passport-local'),
+      passportLocalMongoose = require('passport-local-mongoose'),
+      methodOverride        = require('method-override'),
+      // Routes:
+      postRoutes = require('./routes/posts'),
+      profileRoutes = require('./routes/profile'),
+      indexRoutes = require('./routes/index');
 
 const app = express();
 
+// Config:
 app.set('view engine', 'ejs');
+app.use(require('express-session')({
+    secret: 'Helping people is fun',
+    resave: false,
+    saveUninitialized: false
+}));
 app.use(express.static(__dirname + '/public'));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(methodOverride('_method'));
 
-app.get('/', function (req, res) {
-    res.render('index');
+// passport auth:
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// middleware to pass user into each route
+app.use(function (req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
+
+// requiring routes
+app.use(indexRoutes);
+app.use(postRoutes);
+app.use(profileRoutes);
+
+// MongoDB set-up
+mongoose.connect('mongodb://localhost:27017/blochacks', {useNewUrlParser:true});
+
+
+// 404 error
+app.use(function (req, res, next) {
+    res.status(404).render('error');
+});
+
+// 500 error
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!');
 });
 
 app.listen(3000, function (){
