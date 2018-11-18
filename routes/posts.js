89e32850418ce0a256b88625fa711ce044fa12d7
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2018-11-17 21:27:42 -0500

    file upload

--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,49 @@ const router = app.Router();
 const userDb = require("../db_interactions/user");
 const postDb = require("../db_interactions/post");
 
+//Middleware
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
+
+// img upload storage engine
+var storage = multer.diskStorage({
+    destination: './public/uploads',
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+// img init upload
+var upload = multer({
+    storage: storage,
+    limits: {fileSize: 1000000},
+    fileFilter: function (req, file, cb) {
+        checkFileType(file, cb);
+    }
+}).single('newPost');
+
+function checkFileType (file, cb) {
+    // Allowed extensions
+    var filetypes = /jpeg|jpg|png|gif/;
+    // check extensions
+    var extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // check mime type
+    var mimetype = filetypes.test(file.mimetype);
+    if (extname && extname) {
+        return cb(null, true);
+    }
+    cb('Error: Images only');
+}
+
+
+//Routes
+router.get("/new", (req, res) => {
+    res.render('newPost');
+});
 
 router.post("/", function(req,res){
     let newPost = {
